d0de78cf28749a380edb7f700debb3f7608f7aaa
Roman Troshin <troshinrabota@gmail.com>
2023-11-09 19:02:43 +0300

    rewrite without JSX

--- my/src/App.js
+++ my/src/App.js
@@ -1,36 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 function CreateDate() {
-	const dateYear = new Date().getFullYear(); // во всем файле декларативный подход
+	const dateYear = new Date().getFullYear();
 
-	return (
-		<p className='todayYear'>
-			{dateYear}
-		</p>
-	)
+	return createElement('p', { className: 'todayYear' }, `${dateYear}`);
 }
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<CreateDate/>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: `${logo}`,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement(CreateDate),
+		),
 	);
 };
-
-
